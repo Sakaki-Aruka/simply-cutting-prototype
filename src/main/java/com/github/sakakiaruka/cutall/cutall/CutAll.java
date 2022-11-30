905ac2c25ab19ed9d9ca0a36cdfaaf8dac586e72
Aruka <yshida1pc7@gmail.com>
2022-11-30 10:48:35 +0900

    update : register Events

--- src/main/java/com/github/sakakiaruka/cutall/cutall/CutAll.java
+++ src/main/java/com/github/sakakiaruka/cutall/cutall/CutAll.java
@@ -1,5 +1,6 @@
 package com.github.sakakiaruka.cutall.cutall;
 
+import com.github.sakakiaruka.cutall.cutall.listener.blockBreak;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CutAll extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class CutAll extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new blockBreak(),this);
     }
 
     @Override
